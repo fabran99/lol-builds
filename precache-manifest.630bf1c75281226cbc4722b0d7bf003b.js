f6009d11259730250c4ddf8e6d762d6a71528b99
fabran99 <46858770+fabran99@users.noreply.github.com>
2019-04-19 20:45:27 -0300

    Add files via upload

---
+++ precache-manifest.630bf1c75281226cbc4722b0d7bf003b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea683c0c38e54716a743",
+    "url": "/lol-builds/static/js/runtime~main.84e1de9c.js"
+  },
+  {
+    "revision": "e69553cda2c5e907d420",
+    "url": "/lol-builds/static/js/main.5146334f.chunk.js"
+  },
+  {
+    "revision": "7f12a2750a80990a1e5c",
+    "url": "/lol-builds/static/js/2.dd49378e.chunk.js"
+  },
+  {
+    "revision": "e69553cda2c5e907d420",
+    "url": "/lol-builds/static/css/main.423d34d9.chunk.css"
+  },
+  {
+    "revision": "7e5a28ef8bd0e12667bcf9ff7815676b",
+    "url": "/lol-builds/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lol-builds/precache-manifest.8c5e2105e8cd87732980a8da104cd1c5.js"
+  "/lol-builds/precache-manifest.630bf1c75281226cbc4722b0d7bf003b.js"
 );
 
 workbox.clientsClaim();
