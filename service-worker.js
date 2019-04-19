23a3ef60e933b40cd749828915e44f7bc1530104
fabran99 <46858770+fabran99@users.noreply.github.com>
2019-04-19 17:04:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a842bdfeaf8cb64b02b3042b2c3e241e.js"
+  "/lol-builds/precache-manifest.a842bdfeaf8cb64b02b3042b2c3e241e.js"
 );
 
 workbox.clientsClaim();
